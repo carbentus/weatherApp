3c84a6bc1deeb42ad7ba889923fababb72da6f57
Karina Dalileh <karinad89@gmail.com>
2021-05-09 21:32:16 +0200

    Updates

---
+++ precache-manifest.faf27a1f06599fc32f76a57c9cdf6157.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e71ee027657ef1b1352f",
+    "url": "/weatherApp/static/css/main.6c0816bc.chunk.css"
+  },
+  {
+    "revision": "e71ee027657ef1b1352f",
+    "url": "/weatherApp/static/js/main.24a5bfdb.chunk.js"
+  },
+  {
+    "revision": "b0ae699ea3cc63fef9c8",
+    "url": "/weatherApp/static/js/runtime~main.25533534.js"
+  },
+  {
+    "revision": "29c2c9fd081770e404bd",
+    "url": "/weatherApp/static/js/2.68673e03.chunk.js"
+  },
+  {
+    "revision": "2268cc49deae69ee102d790b49b0326a",
+    "url": "/weatherApp/index.html"
+  }
+];
